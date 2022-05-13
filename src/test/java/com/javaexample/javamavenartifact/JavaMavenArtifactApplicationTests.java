8f64832b8582170caf0b7c5eba562fdc843c9ff5
shivam <tomar102003@gmail.com>
2022-05-13 22:46:24 +0530

    adding codes

---
+++ src/test/java/com/javaexample/javamavenartifact/JavaMavenArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaexample.javamavenartifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMavenArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
